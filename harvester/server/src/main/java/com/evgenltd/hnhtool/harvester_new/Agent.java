7e984725750273e020bcd83405eda26c1eb5f0cf
Evgeniy Lebedev <evgeniy.lebedev@magenta-technology.com>
2019-11-25 21:40:42 +0400

    HNHT-2 Complex client improvements

---
+++ harvester/server/src/main/java/com/evgenltd/hnhtool/harvester_new/Agent.java
@@ -0,0 +1,9 @@
+package com.evgenltd.hnhtool.harvester_new;
+
+/**
+ * Project: hnhtool-root
+ * Author:  Lebedev
+ * Created: 25-11-2019 18:41
+ */
+public interface Agent {
+}
---
+++ harvester/server/src/main/java/com/evgenltd/hnhtool/harvester_new/AgentService.java
@@ -0,0 +1,26 @@
+package com.evgenltd.hnhtool.harvester_new;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Project: hnhtool-root
+ * Author:  Lebedev
+ * Created: 25-11-2019 18:27
+ */
+public interface AgentService {
+
+    @NotNull
+    Long scheduleScriptExecution(@NotNull Script script);
+
+    @NotNull
+    ExecutionStatus getScriptExecutionStatus(@NotNull Long scheduleId);
+
+    enum ExecutionStatus {
+        NOT_FOUND,
+        QUEUED,
+        IN_PROGRESS,
+        COMPLETED,
+        FAILED
+    }
+
+}
