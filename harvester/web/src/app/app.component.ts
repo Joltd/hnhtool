5e56952e2230b6b90638958fcaca5f964571071c
Evgeniy Lebedev <lebedev1537@gmail.com>
2020-08-08 23:54:31 +0400

    Code cleanup

--- harvester/web/src/app/app.component.ts
+++ harvester/web/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
@@ -8,20 +6,10 @@ import {environment} from "../environments/environment";
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
-  collapsed: boolean;
-
-  constructor(private http: HttpClient) {}
+  collapsed: boolean = true;
 
   toggleCollapsed() {
     this.collapsed = !this.collapsed;
-  }
-
-  error() {
-    this.http.get(environment.apiUrl + '/test/error').subscribe(() => {});
-  }
-
-  success() {
-    this.http.get(environment.apiUrl + '/test/success').subscribe((result) => alert(result));
   }
 
 }
